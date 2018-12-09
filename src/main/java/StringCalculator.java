428154d537f263bb95034bea11056023b0cfd4fd
Christian Soler <solero93@gmail.com>
2018-12-09 18:37:19 +0100

    Added support for any kind of character delimiter

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,19 +1,23 @@
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int Add(String stringWithNumbersToSum) {
         if (stringWithNumbersToSum.equals(""))
             return 0;
 
-        boolean isDelimiterDefined = stringWithNumbersToSum.startsWith("//;\n");
+        boolean isDelimiterDefined = stringWithNumbersToSum.startsWith("//");
 
         String delimiter = ",";
 
         if (isDelimiterDefined) {
-            stringWithNumbersToSum = stringWithNumbersToSum.replaceAll("//;\n", "");
-            delimiter = ";";
+            stringWithNumbersToSum = stringWithNumbersToSum.replaceFirst("//", "");
+            int indexOfFirstNewLine = stringWithNumbersToSum.indexOf("\n");
+            delimiter = stringWithNumbersToSum.substring(0, indexOfFirstNewLine);
+            stringWithNumbersToSum = stringWithNumbersToSum.substring(indexOfFirstNewLine+1);
         }
 
         stringWithNumbersToSum = stringWithNumbersToSum.replace("\n", delimiter);
-        String[] numbersAsStringToSum = stringWithNumbersToSum.split(delimiter);
+        String[] numbersAsStringToSum = stringWithNumbersToSum.split(Pattern.quote(delimiter));
 
         int sumOfNumbers = 0;
         for (String numberAsString : numbersAsStringToSum) {
